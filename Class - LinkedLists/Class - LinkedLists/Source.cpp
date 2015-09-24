20242a24f2ccc026cfd2ac6cfd7b53b81f927626
Nathan Borup <nathan.borup@gmail.com>
2015-09-23 18:46:34 -0600

    Iterators - Adding iterators to the LinkedList class

--- Class - LinkedLists/Class - LinkedLists/Source.cpp
+++ Class - LinkedLists/Class - LinkedLists/Source.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <conio.h>
+#include <forward_list>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+template <typename T> class LinkedList;
+template <typename T> class Node;
+template <typename T> class LinkedListIterator;
 
 template <typename T>
 class Node {
@@ -9,6 +16,19 @@ public:
 	T data;
 	Node* forward;
 	Node* backward;
+};
+
+
+
+template<typename T>
+class LinkedListIterator {
+	friend class LinkedList<T>;
+public:
+	LinkedListIterator<T> operator++();
+	bool operator!=();
+	T operator*();
+private:
+	Node<T>* ptr;
 };
 
 template <typename T>
@@ -21,6 +41,10 @@ public:
 	void deleteFirst();
 	void deleteLast();
 	void deleteItem(const T& item);
+
+	LinkedListIterator<T> begin();
+	LinkedListIterator<T> end();
+
 private:
 	Node<T>* front;
 	Node<T>* back;
@@ -174,37 +198,114 @@ void LinkedList<T>::deleteItem(const T& item) {
 	}
 }
 
+template <typename T>
+LinkedListIterator<T> LinkedList<T>::begin() {
+	LinkedListIterator<T> retVal;
+	retVal.ptr = front;
+
+	return retVal;
+}
+
 int main() {
+	//
+	////Node<int>* n1 = new Node<int>();
 
-	//Node<int>* n1 = new Node<int>();
+	////n1->data = 42; // same as (*n1).data = 42;
+	////n1->link = NULL;
 
-	//n1->data = 42; // same as (*n1).data = 42;
-	//n1->link = NULL;
+	////Node<int>* n2 = new Node<int>();
+	////n2->data = 12;
+	////n1->link = n2;
 
-	//Node<int>* n2 = new Node<int>();
-	//n2->data = 12;
-	//n1->link = n2;
+	////Node<int>* n3 = new Node<int>();
+	////n2->data = 84;
+	////n2->link = n3;
+	////n3->link = NULL;
 
-	//Node<int>* n3 = new Node<int>();
-	//n2->data = 84;
-	//n2->link = n3;
-	//n3->link = NULL;
+	//////Get rid of middle node
+	////n1->link = n3;
+	////delete n2;
+	////n2 = NULL;
 
-	////Get rid of middle node
-	//n1->link = n3;
-	//delete n2;
-	//n2 = NULL;
+	///*LinkedList<int> mylist;
+	//mylist.insertLast(42);
+	//mylist.insertLast(12);
+	//mylist.insertLast(84);
+	//mylist.insertFirst(32);
+	//mylist.insertFirst(25);
+	//mylist.insertFirst(22);
+	//mylist.deleteItem(12);
+	//mylist.deleteItem(84);
+	//mylist.deleteItem(22);*/
 
-	LinkedList<int> mylist;
-	mylist.insertLast(42);
-	mylist.insertLast(12);
-	mylist.insertLast(84);
-	mylist.insertFirst(32);
-	mylist.insertFirst(25);
-	mylist.insertFirst(22);
-	mylist.deleteItem(12);
-	mylist.deleteItem(84);
-	mylist.deleteItem(22);
+	///*******Iterators********/
+	//int * arr = new int[10];
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	arr[i] = i;
+	//}
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cout << i << endl;
+	//}
+	//cout << "Forward List" << endl;
+	//forward_list<int> l;
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	l.push_front(i);
+	//}
+
+	///*for (int i = 0; i < 10; i++)
+	//{
+	//	cout << l.front() << endl;
+	//	l.pop_front();
+	//}*/
+	////Iterator example - allows you to read the data multiple times without popping values
+	///*for (forward_list<int>::const_iterator iter = l.begin();
+	//	iter != l.end();++iter)
+	//{
+	//	cout << *iter << endl;
+	//}*/
+	//for (auto i : l) //Does the same as above
+	//{
+	//	cout << i << endl;
+	//}
+
+	//cout << "Vector List" << endl;
+	//vector<int> v;
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	v.push_back(i);
+	//}
+	////for (int i = 0; i < v.size(); i++)
+	////{
+	////	cout << v[i] << endl;
+	////}
+	////for (vector<int>::const_iterator iter = v.begin(); iter != v.end(); ++iter)
+	////{
+	////	cout << *iter << endl;
+	////}
+	////for (auto iter = v.begin(); iter != v.end(); ++iter)//Does the same as above
+	////{
+	////	cout << *iter << endl;
+	////}
+	//for (auto i : v) //Does the same as above
+	//{
+	//	cout << i << endl;
+	//}
+	////Reverses the range of data
+	//reverse(v.begin(), v.end());
+	//for (auto i : v)
+	//{
+	//	cout << i << endl;
+	//}
+	////Sorts range data
+	//sort(v.begin(), v.end());
+	//for (auto i : v)
+	//{
+	//	cout << i << endl;
+	//}
+	//
 
 	_getch();
 	return 0;
@@ -212,4 +313,5 @@ int main() {
 	// 9/14 participation code 841242
 	// 9/16 participation code 536837
 	// 9/21 participation code 431872
+	// 9/23 participation code 323232
 }
